f49860eef9acbef97fc61bf6c66d11f776d819ae
S-Oleksii <shapovaloleksii14@gmail.com>
2022-10-03 11:35:35 +0300

    Lab_4

---
+++ index.js
@@ -0,0 +1,323 @@
+console.log('-----------------------Task 1--------------------------')
+
+let a_1 = 1, a_2 = 0, a_3 = -3;
+
+  if(a_1 == 0){
+    console.log("Вірно")
+  }
+  else
+    console.log("Невірно")
+
+  if(a_2 == 0){
+    console.log("Вірно")
+  }
+ else
+    console.log("Невірно")
+
+  if(a_3 == 0){
+      console.log("Вірно")
+    }
+  else
+      console.log("Невірно")
+ console.log('\n\n')
+
+ console.log('-----------------------Task 2--------------------------')
+
+  if(a_1 > 0){
+    console.log("Вірно")
+  }
+  else
+    console.log("Невірно")
+
+  if(a_2 > 0){
+    console.log("Вірно")
+  }
+  else
+    console.log("Невірно")
+
+  if(a_3 > 0){
+      console.log("Вірно")
+    }
+  else
+      console.log("Невірно")
+ console.log('\n\n')
+
+ console.log('-----------------------Task 3--------------------------')
+
+  if(a_1 < 0){
+    console.log("Вірно")
+  }
+  else
+    console.log("Невірно")
+
+  if(a_2 < 0){
+    console.log("Вірно")
+  }
+  else
+    console.log("Невірно")
+
+  if(a_3 < 0){
+      console.log("Вірно")
+    }
+  else
+      console.log("Невірно")
+ console.log('\n\n')
+
+ console.log('-----------------------Task 4--------------------------')
+
+  if(a_1 >= 0){
+    console.log("Вірно")
+  }
+  else
+    console.log("Невірно")
+
+  if(a_2 >= 0){
+    console.log("Вірно")
+  }
+  else
+    console.log("Невірно")
+
+  if(a_3 >= 0){
+      console.log("Вірно")
+    }
+  else
+      console.log("Невірно")
+ console.log('\n\n')
+
+ console.log('-----------------------Task 5--------------------------')
+
+  if(a_1 <= 0){
+    console.log("Вірно")
+  }
+  else
+    console.log("Невірно")
+
+  if(a_2 <= 0){
+    console.log("Вірно")
+  }
+  else
+    console.log("Невірно")
+
+  if(a_3 <= 0){
+      console.log("Вірно")
+    }
+  else
+      console.log("Невірно")
+ console.log('\n\n')
+
+ console.log('-----------------------Task 6--------------------------')
+
+  if(a_1 != 0){
+    console.log("Вірно")
+  }
+  else
+    console.log("Невірно")
+
+  if(a_2 != 0){
+    console.log("Вірно")
+  }
+  else
+    console.log("Невірно")
+
+  if(a_3 != 0){
+      console.log("Вірно")
+    }
+  else
+      console.log("Невірно")
+ console.log('\n\n')
+
+ console.log('-----------------------Task 7--------------------------')
+
+ let A_1 = 'test', A_2 = 'тест', A_3 = 3;
+
+  if(A_1 === 'test'){
+    console.log("Вірно")
+  }
+  else
+    console.log("Невірно")
+
+  if(A_2 ==='test'){
+    console.log("Вірно")
+  }
+  else
+    console.log("Невірно")
+
+  if(A_3 === 'test'){
+      console.log("Вірно")
+    }
+  else
+      console.log("Невірно")
+ console.log('\n\n')
+
+ console.log('-----------------------Task 8--------------------------')
+
+ let a1 = '1', a2 = 1, a3 = 3;
+
+  if(a1 === '1' && (typeof a1) ===(typeof '1')){
+    console.log("Вірно")
+  }
+  else
+    console.log("Невірно")
+
+  if(a2 === '1' && (typeof a2) ===(typeof '1')){
+    console.log("Вірно")
+  }
+  else
+    console.log("Невірно")
+
+  if(a3 === '1' && (typeof a3) ===(typeof '1')){
+      console.log("Вірно")
+    }
+  else
+      console.log("Невірно")
+ console.log('\n\n')
+
+ console.log('-----------------------Task 9--------------------------')
+
+ let test_1 = true, test_2 = false;
+
+  if(test_1 == true ){
+    console.log("Вірно")
+  }
+  else
+    console.log("Невірно")
+
+  if(test_2 == true){
+    console.log("Вірно")
+  }
+  else
+    console.log("Невірно")
+ console.log('\n\n')
+
+ console.log('-----------------------Task 10--------------------------')
+
+ if(test_1 != true ){
+  console.log("Вірно")
+}
+else
+  console.log("Невірно")
+
+if(test_2 != true){
+  console.log("Вірно")
+}
+else
+  console.log("Невірно")
+console.log('\n\n')
+
+console.log('-----------------------Task 11--------------------------')
+
+let A1 = 5, A2 = 0, A3 = -3, A4 = 2;
+
+if(A1 > 0 && A1 < 5){
+    console.log("Вірно")
+  }
+else
+    console.log("Невірно")
+
+if(A2 > 0 && A2 < 5){
+  console.log("Вірно")
+    }
+else
+ console.log("Невірно")
+
+if(A3 > 0 && A3 < 5){
+  console.log("Вірно")
+      }
+else
+  console.log("Невірно")
+if(A4 > 0 && A4 < 5){
+  console.log("Вірно")
+        }
+else
+  console.log("Невірно")
+ console.log('\n\n')
+
+ console.log('-----------------------Task 12--------------------------')
+
+if(A1 == 0 || A1 == 2){
+  console.log(A1+7)
+}
+else
+  console.log(A1/10)
+
+if(A2 == 0 || A2 == 2){
+  console.log(A2+7)
+  }
+else
+  console.log(A2/10)
+
+if(A3 == 0 || A3 == 2){
+  console.log(A3+7)
+    }
+else
+  console.log(A3/10)
+
+if(A4 == 0 || A4 == 2){
+  console.log(A4+7)
+      }
+else
+  console.log(A4/10)
+console.log('\n\n')
+
+console.log('-----------------------Task 13--------------------------')
+
+let a1_ = 1, b1_ = 3;
+let a2_ = 0, b2_ = 6;
+let a3_ = 3, b3_ = 5;
+
+if(a1_ <=1 && b1_ >=3)
+ console.log(a1_+b1_)
+else 
+ console.log(a1_-b1_)
+
+if(a2_ <= 1 && b2_ >= 3)
+ console.log(a2_+b2_)
+else 
+ console.log(a2_-b2_)
+
+if(a3_ <=1 && b3_ >=3)
+ console.log(a3_+b3_)
+else 
+ console.log(a3_-b3_)
+console.log('\n\n')
+
+console.log('-----------------------Task 14--------------------------')
+
+if((a1_ > 2 && a1_ < 11)||(b1_ >= 6 && b1_ < 14))
+ console.log('Вірно')
+else 
+ console.log('Невірно')
+
+ if((a2_ > 2 && a2_ < 11)||(b2_ >= 6 && b2_ < 14))
+ console.log('Вірно')
+else 
+ console.log('Невірно')
+
+ if((a3_ > 2 && a3_ < 11)||(b3_ >= 6 && b3_ < 14))
+ console.log('Вірно')
+else 
+ console.log('Невірно')
+console.log('\n\n')
+
+console.log('-----------------------Task 15--------------------------')
+
+let num = 4, result
+
+switch (num) {
+  case 1:
+    result = 'Зима'
+    console.log( result );
+     break
+     case 2:
+      result = 'Весна'
+      console.log( result );
+       break
+       case 3:
+        result = 'Літо'
+        console.log( result );
+         break
+         case 4:
+          result = 'Осінь'
+           console.log( result );
+           break
+}
